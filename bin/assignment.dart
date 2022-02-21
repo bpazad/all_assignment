b7fb2581c7d7c461b576de4d5bfa33199e257efa
ABUL KALAM AZAD <circleup.kalam@gmail.com>
2022-02-21 23:50:31 +0600

    assignment01

---
+++ bin/assignment.dart
@@ -0,0 +1,27 @@
+
+void main() {
+
+
+  ///Ans to the Question No: 1 ///
+
+  String first = "Hello";
+  String name = "Abul Kalam Azad";
+
+  print("$first \nMy Name is $name");
+
+
+
+
+
+  /// Ans to the Question No: 2 ///
+
+  int Num1 = 237;
+  int Num2 = 263;
+  int sum = Num1 + Num2;
+
+  print("Total: $Num1+$Num2 = $sum");
+
+
+
+
+}
